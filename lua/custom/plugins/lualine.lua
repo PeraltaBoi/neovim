a3b03f3b4fbeb0b9c4c4e3519e0bef7ce85adbbf
PeraltaBoi <tiagofilipeperalta@gmail.com>
2024-11-14 09:57:31 +0000

    update ui

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
       options = {
         icons_enabled = true,
         -- theme = 'auto',
-        theme = 'catppuccin',
+        theme = 'kanagawa',
         component_separators = { left = '', right = '' },
         -- section_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
--- lua/custom/themes/catppuccin.lua
+++ lua/custom/themes/catppuccin.lua
@@ -1,30 +1,31 @@
-return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    require('catppuccin').setup {
-      native_lsp = {
-        enabled = true,
-        virtual_text = {
-          errors = { 'italic' },
-          hints = { 'italic' },
-          warnings = { 'italic' },
-          information = { 'italic' },
-          ok = { 'italic' },
-        },
-        underlines = {
-          errors = { 'underline' },
-          hints = { 'underline' },
-          warnings = { 'underline' },
-          information = { 'underline' },
-          ok = { 'underline' },
-        },
-        inlay_hints = {
-          background = true,
-        },
-      },
-    }
-  end,
-}
--- return { 'typicode/bg.nvim', lazy = false }
+return {}
+-- return {
+--   'catppuccin/nvim',
+--   name = 'catppuccin',
+--   priority = 1000,
+--   config = function()
+--     require('catppuccin').setup {
+--       native_lsp = {
+--         enabled = true,
+--         virtual_text = {
+--           errors = { 'italic' },
+--           hints = { 'italic' },
+--           warnings = { 'italic' },
+--           information = { 'italic' },
+--           ok = { 'italic' },
+--         },
+--         underlines = {
+--           errors = { 'underline' },
+--           hints = { 'underline' },
+--           warnings = { 'underline' },
+--           information = { 'underline' },
+--           ok = { 'underline' },
+--         },
+--         inlay_hints = {
+--           background = true,
+--         },
+--       },
+--     }
+--   end,
+-- }
+-- -- return { 'typicode/bg.nvim', lazy = false }

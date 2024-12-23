e620439721229c1f49b49c9797ffc80342a4307f
PeraltaBoi <tiagofilipeperalta@gmail.com>
2024-12-23 20:08:49 +0000

    blink and some other stuff

--- lua/custom/plugins/lsp/lspconfig.lua
+++ lua/custom/plugins/lsp/lspconfig.lua
@@ -28,6 +28,9 @@ return {
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
+
+      -- Blink config
+      'saghen/blink.cmp',
     },
     config = function()
       -- Brief aside: **What is LSP?**
@@ -156,6 +159,7 @@ return {
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -223,19 +227,7 @@ return {
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-        'ts_ls',
-        'prettierd',
-        'eslint',
-        'tailwindcss',
-        'svelte',
-        'gopls',
-        'jsonls',
-        'html',
-        'markdownlint',
-        'zls',
-      })
+      vim.list_extend(ensure_installed, {})
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
